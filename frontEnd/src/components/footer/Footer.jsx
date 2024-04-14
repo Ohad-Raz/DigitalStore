85713f34af14612a84f04f016a42dff5bb1b9ca7
Ohad-Raz <ohadraz12@gmail.com>
2024-04-14 14:28:13 +0300

    responsive footer, shopping cart cards, btnsMargin

--- frontEnd/src/components/footer/Footer.jsx
+++ frontEnd/src/components/footer/Footer.jsx
@@ -8,11 +8,12 @@ import {
   MDBRow,
   MDBBtn
 } from 'mdb-react-ui-kit';
+import  "./Footer.css"
 
 export default function Footer() {
   return (
     <MDBFooter className='text-center' color='white' bgColor='dark'>
-      <MDBContainer className='p-4'>
+<MDBContainer className='p-4' style={{ marginLeft: 'unset' }}>
         <section className='mb-4'>
           <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
             <MDBIcon fab icon='facebook-f' />
