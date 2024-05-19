78acc015aac52054f47a49afdf4466cd3e410deb
HilaZagdon <hilatoar@gmail.com>
2024-05-19 22:44:53 +0300

    home style

--- frontEnd/src/components/siteInfo/SiteInfo.jsx
+++ frontEnd/src/components/siteInfo/SiteInfo.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import styles from './SiteInfo.module.css';
 import { Link } from 'react-router-dom';
+import RodovLogo from "../../assets/RODOV CENTER ELECTRICAL BUILDING & INDUSTRIAL PRODUCTS.png"
 function SiteInfo() {
   return (
+    <div>
+      <img src={RodovLogo} className={styles.RodovLogo} alt="" />
+
     <div className={styles.container}>
+    
       <div className={styles.content}>
-        <h2>About RODOV CENTER ELECTRICAL BUILDING & INDUSTRIAL PRODUCTS</h2>
-        <p>Welcome to RODOV CENTER, your premier destination for electrical, electronic, and industrial products. With a rich history and a commitment to excellence, we strive to meet the diverse needs of our customers in the electrical and industrial sectors.</p>
-        
+        <div className={styles.contentText}>
+        <div className={styles.leftContent}>
+        {/* <p className={styles.welcome}>Welcome to RODOV CENTER, your premier destination for electrical, electronic, and industrial products. With a rich history and a commitment to excellence, we strive to meet the diverse needs of our customers in the electrical and industrial sectors.</p> */}
         <h2>Our History</h2>
         <p>RODOV CENTER was founded in [year] with a vision to provide high-quality electrical and industrial products to businesses and individuals alike. Over the years, we have grown into a trusted name in the industry, known for our reliability, expertise, and dedication to customer satisfaction.</p>
         
         <h2>Fields of Activity</h2>
         <p>At RODOV CENTER, we specialize in a wide range of fields, including:</p>
+        <h2>Our Mission</h2>
+        <p>At RODOV CENTER, our mission is to deliver superior products and services that exceed our customers' expectations. We are committed to innovation, quality, and sustainability in everything we do.</p>
+        
+        <h2>Contact Us</h2>
+        <p>Thank you for choosing RODOV CENTER for your electrical and industrial needs. For inquiries, orders, or assistance, please don't hesitate to 
+        <Link  style={{ color: 'black'}} to={"/contact"}> contact us</Link>.</p>
+        </div>
+        <div className={styles.rightContent}>
         <section className={styles.section}>
         <ul>
         <li><Link  style={{ textDecoration: 'none'}} to={"/catalog"}>Electrical and Electronic Equipment</Link></li>
@@ -25,13 +38,11 @@ function SiteInfo() {
           <li><Link  style={{ textDecoration: 'none'}} to={"/catalog"}>Specialty Products</Link></li>
         </ul>
         </section>
-        <h2>Our Mission</h2>
-        <p>At RODOV CENTER, our mission is to deliver superior products and services that exceed our customers' expectations. We are committed to innovation, quality, and sustainability in everything we do.</p>
-        
-        <h2>Contact Us</h2>
-        <p>Thank you for choosing RODOV CENTER for your electrical and industrial needs. For inquiries, orders, or assistance, please don't hesitate to 
-        <Link  style={{ color: 'black'}} to={"/contact"}> contact us</Link>.</p>
+        </div>
+        </div>
+
       </div>
+    </div>
     </div>
   );
 }
